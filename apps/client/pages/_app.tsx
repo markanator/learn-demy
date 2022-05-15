00cc5f979f5561d6156e551b31d451fee793a8ed
Mark Ambro <wowmarkambro@gmail.com>
2022-05-14 21:50:42 -0600

    create server

--- apps/client/pages/_app.tsx
+++ apps/client/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import './styles.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/index.css';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
