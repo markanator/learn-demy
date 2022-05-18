56d893d31c890925b03fdf02f77998b69c69775f
Mark Ambro <wowmarkambro@gmail.com>
2022-05-18 01:54:53 -0600

    rq devTools, fix toasts

--- apps/client/pages/_app.tsx
+++ apps/client/pages/_app.tsx
@@ -6,16 +6,18 @@ import '../styles/index.css';
 import TopNav from '../components/TopNav';
 import AuthProvider from '../context/auth.context';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 const client = new QueryClient();
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={client}>
+      <ReactQueryDevtools />
       <AuthProvider>
         <TopNav />
         <Component {...pageProps} />
-        <ToastContainer position="top-center" />
+        <ToastContainer position="top-center" hideProgressBar closeOnClick />
       </AuthProvider>
     </QueryClientProvider>
   );
