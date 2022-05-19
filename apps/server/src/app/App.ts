465458655ab097b03bdcac0ceabaff7e26d74ce0
Mark Ambro <wowmarkambro@gmail.com>
2022-05-19 01:12:22 -0600

    enhance server

---
+++ apps/server/src/app/App.ts
@@ -0,0 +1,46 @@
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import { config } from 'dotenv';
+import csrf from 'csurf';
+import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
+import compression from 'compression';
+// LOCAL IMPORTS
+import AuthRoutes from '../routes/auth.routes';
+import InstructorRoutes from '../routes/Instructor.routes';
+import CourseRoutes from '../routes/course.routes';
+import { __prod__ } from '../utils/env.utils';
+
+config();
+
+const csrfProtection = csrf({ cookie: true });
+const app = express();
+// MW
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.CORS_ORIGIN,
+  })
+);
+app.use(helmet());
+app.use(cookieParser());
+app.use(express.json({ limit: '2mb' }));
+app.use(compression());
+if (!__prod__) {
+  app.use(morgan('dev'));
+}
+// routes
+app.get('/', (_, res) => res.send('Server up!'));
+app.use('/auth', AuthRoutes);
+app.use('/instructors', InstructorRoutes);
+app.use('/courses', CourseRoutes);
+
+app.use(csrfProtection);
+app.get('/csrf-token', (req, res) =>
+  res.json({
+    csrfToken: req.csrfToken(),
+  })
+);
+
+export default app;
--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,56 +1,20 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import express from 'express';
-import cors from 'cors';
-import morgan from 'morgan';
-import { config } from 'dotenv';
 import mongoose from 'mongoose';
-import csrf from 'csurf';
-import cookieParser from 'cookie-parser';
-// LOCAL IMPORTS
-import AuthRoutes from './routes/auth.routes';
-import InstructorRoutes from './routes/Instructor.routes';
-import CourseRoutes from './routes/course.routes';
+import App from './app/App';
 
-config();
+const port = process.env.PORT || 3333;
 
-const csrfProtection = csrf({ cookie: true });
-const app = express();
-// MW
-app.use(
-  cors({
-    credentials: true,
-    origin: process.env.CORS_ORIGIN,
-  })
-);
-app.use(cookieParser());
-app.use(express.json({ limit: '2mb' }));
-app.use(morgan('dev'));
+function main() {
+  mongoose
+    .connect(process.env.DB_URL, {})
+    .then(() => console.info('DB connected'))
+    .catch((err) => console.error('DB Error => ', err));
 
-// MONGOOSE
-mongoose
-  .connect(process.env.DB_URL, {})
-  .then(() => console.info('DB connected'))
-  .catch((err) => console.error('DB Error => ', err));
+  App.listen(port, () => {
+    console.info(`Listening at http://localhost:${port}`);
+  }).on('error', (err) => {
+    console.error(err);
+    process.exit(1);
+  });
+}
 
-// routes
-app.get('/', (_, res) => res.send('Server up!'));
-app.use('/auth', AuthRoutes);
-app.use('/instructors', InstructorRoutes);
-app.use('/courses', CourseRoutes);
-
-app.use(csrfProtection);
-app.get('/csrf-token', (req, res) =>
-  res.json({
-    csrfToken: req.csrfToken(),
-  })
-);
-
-const port = process.env.port || 3333;
-const server = app.listen(port, () => {
-  console.info(`Listening at http://localhost:${port}`);
-});
-server.on('error', console.error);
+main();
